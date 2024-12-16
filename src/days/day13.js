22b48a0a921181cbbb24b92a46d67b577f5e7f09
Jose Angel "Casa <jangelsalinas@gmail.com>
2024-12-16 19:08:05 +0100

    Refactor isRobotBack function to improve movement handling and add support for conditional movements

--- src/days/day13.js
+++ src/days/day13.js
@@ -32,92 +32,120 @@ function isRobotBack(moves) {
     let y = 0;
     let invert = false;
     let lastMove = '';
+    let uMovementDone = false;
+    let dMovementDone = false;
+    let lMovementDone = false;
+    let rMovementDone = false;
     for (let i = 0; i < moves.length; i++) {
         let move = moves[i];
         if (move === 'R') {
             if (invert) {
                 x--;
+                lMovementDone = true;
             } else {
                 x++;
+                rMovementDone = true;
             }
         } else if (move === 'L') {
             if (invert) {
                 x++;
+                rMovementDone = true;
             } else {
                 x--;
+                lMovementDone = true;
             }
         } else if (move === 'U') {
             if (invert) {
                 y--;
+                dMovementDone = true;
             } else {
                 y++;
+                uMovementDone = true;
             }
         } else if (move === 'D') {
             if (invert) {
                 y++;
+                uMovementDone = true;
             } else {
                 y--;
+                dMovementDone = true;
             }
+            dMovementDone = true;
         } else if (move === '*') {
             let nextMove = moves[++i];
                 if (nextMove === 'R') {
                     if (invert) {
                         x -= 2;
+                        lMovementDone = true;
                     } else {
                         x += 2;
+                        rMovementDone = true;
                     }
                 } else if (nextMove === 'L') {
                     if (invert) {
                         x += 2;
+                        rMovementDone = true;
                     } else {
                         x -= 2;
+                        lMovementDone = true;
                     }
                 } else if (nextMove === 'U') {
                     if (invert) {
                         y -= 2;
+                        dMovementDone = true;
                     } else {
                         y += 2;
+                        uMovementDone = true;
                     }
                 } else if (nextMove === 'D') {
                     if (invert) {
                         y += 2;
+                        uMovementDone = true;
                     } else {
                         y -= 2;
+                        dMovementDone = true;
                     }
                 }
         } else if (move === '!') {
             invert = !invert;
         } else if (move === '?') {
-            if (lastMove !== moves[i + 1]) {
-                let nextMove = moves[++i];
-                if (nextMove === 'R') {
-                    if (invert) {
-                        x--;
-                    } else {
-                        x++;
-                    }
-                } else if (nextMove === 'L') {
-                    if (invert) {
-                        x++;
-                    } else {
-                        x--;
-                    }
-                } else if (nextMove === 'U') {
-                    if (invert) {
-                        y--;
-                    } else {
-                        y++;
-                    }
-                } else if (nextMove === 'D') {
-                    if (invert) {
-                        y++;
-                    } else {
-                        y--;
-                    }
+            let nextMove = moves[++i];
+            if (nextMove === 'R' && rMovementDone === false) {
+                if (invert) {
+                    x--;
+                } else {
+                    x++;
+                }
+            } else if (nextMove === 'L' && lMovementDone === false) {
+                if (invert) {
+                    x++;
+                } else {
+                    x--;
+                }
+            } else if (nextMove === 'U' &&  uMovementDone === false) {
+                if (invert) {
+                    y--;
+                } else {
+                    y++;
+                }
+            } else if (nextMove === 'D' &&  dMovementDone === false) {
+                if (invert) {
+                    y++;
+                } else {
+                    y--;
                 }
             }
         }
-        lastMove = move;
+        if (move === '*') {
+            let nextMove = moves[i];
+            lastMove = nextMove;
+        }else{
+            lastMove = move;
+        }
+
+        if(move !== '!' && invert==true){
+            invert = false;
+        }
     }
     if (x === 0 && y === 0) {
         return true;
@@ -128,28 +156,28 @@ function isRobotBack(moves) {
 
 }
 
-console.log(isRobotBack('R'));     // [1, 0]
-console.log(isRobotBack('RL'));    // true
-console.log(isRobotBack('RLUD'));  // true
-console.log(isRobotBack('*RU'));   // [2, 1]
-console.log(isRobotBack('R*U'));   // [1, 2]
-console.log(isRobotBack('LLL!R')); // [-4, 0]
-console.log(isRobotBack('R?R'));   // [1, 0]
-console.log(isRobotBack('U?D'));   // true
-console.log(isRobotBack('R!L'));   // [2,0]
-console.log(isRobotBack('U!D'));   // [0,2]
-console.log(isRobotBack('R?L'));   // true
-console.log(isRobotBack('U?U'));   // [0,1]
+// console.log(isRobotBack('R'));     // [1, 0]
+// console.log(isRobotBack('RL'));    // true
+// console.log(isRobotBack('RLUD'));  // true
+// console.log(isRobotBack('*RU'));   // [2, 1]
+// console.log(isRobotBack('R*U'));   // [1, 2]
+// console.log(isRobotBack('LLL!R')); // [-4, 0]
+// console.log(isRobotBack('R?R'));   // [1, 0]
+// console.log(isRobotBack('U?D'));   // true
+// console.log(isRobotBack('R!L'));   // [2,0]
+// console.log(isRobotBack('U!D'));   // [0,2]
+// console.log(isRobotBack('R?L'));   // true
+// console.log(isRobotBack('U?U'));   // [0,1]
 console.log(isRobotBack('*U?U'));  // [0,2]
-console.log(isRobotBack('U?D?U')); // true
+// console.log(isRobotBack('U?D?U')); // true
 
 // Ejemplos paso a paso:
-console.log(isRobotBack('R!U?U')); // [1,0]
+// console.log(isRobotBack('R!U?U')); // [1,0]
 // 'R'  -> se mueve a la derecha 
 // '!U' -> se invierte y se convierte en 'D'
 // '?U' -> se mueve arriba, porque no se ha hecho el movimiento 'U'
 
-console.log(isRobotBack('UU!U?D')); // [0,1]
+// console.log(isRobotBack('UU!U?D')); // [0,1]
 // 'U'  -> se mueve arriba
 // 'U'  -> se mueve arriba
 // '!U' -> se invierte y se convierte en 'D'
